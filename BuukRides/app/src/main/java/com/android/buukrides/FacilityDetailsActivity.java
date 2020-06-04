9b3815ada5656ccb55e9e43f2fc1268fcd884315
DenisBiwott <denisbiwott@gmail.com>
2020-06-04 08:01:43 +0300

    Hide book button from venue owner
    Hide book button in facility details from owner because he/she cannot
    book their own venue.

--- BuukRides/app/src/main/java/com/android/buukrides/FacilityDetailsActivity.java
+++ BuukRides/app/src/main/java/com/android/buukrides/FacilityDetailsActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,10 +21,13 @@ import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 public class FacilityDetailsActivity extends AppCompatActivity {
-    private String facility_id, venue_id;
-    private TextView mTxtCapacity, mTxtOpeningTime, mTxtClosingTime, mTxtPrice, mTxtFacilityName;
+    private String facility_id, venue_id, user_id;
+    private TextView mTxtCapacity, mTxtOpeningTime, mTxtClosingTime, mTxtPrice, mTxtFacilityName,
+    mBtnBookFacility;
     private ImageView mImgFacilityImage;
     private DatabaseReference facDatabaseRef;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +40,36 @@ public class FacilityDetailsActivity extends AppCompatActivity {
         mTxtClosingTime = findViewById(R.id.txtClosingTime);
         mTxtPrice = findViewById(R.id.txtPrice);
         mTxtFacilityName = findViewById(R.id.txtFacilityName);
-
+        mBtnBookFacility = findViewById(R.id.btnBookFacility);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        user_id = user.getUid();
 
         venue_id = getIntent().getStringExtra("venue_id");
         facility_id = getIntent().getStringExtra("facility_id");
-        facDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Venues").child(venue_id).child("Facility").child(facility_id);
-        facDatabaseRef.addValueEventListener(new ValueEventListener() {
+        facDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Venues").child(venue_id);
+
+        //Hide Book button if user owns venue
+        facDatabaseRef.child("UserID").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String UserID = (String)dataSnapshot.getValue();
+                if (UserID.equals(user_id)){
+                    mBtnBookFacility.setVisibility(View.GONE);
+                }
+                else {
+                    mBtnBookFacility.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //Populate Facility Activity
+        facDatabaseRef.child("Facility").child(facility_id).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String FacName = (String)dataSnapshot.child("Name").getValue();
@@ -47,7 +77,8 @@ public class FacilityDetailsActivity extends AppCompatActivity {
                 String ClosingTime = (String)dataSnapshot.child("ClosingTime").getValue();
                 String Capacity = (String)dataSnapshot.child("Capacity").getValue();
                 String Cost = (String)dataSnapshot.child("Cost").getValue();
-                final String FacilityImageUrl = (String)dataSnapshot.child("facilityImageUrl").getValue();
+                final String FacilityImageUrl = (String)dataSnapshot.child("facilityImageUrl")
+                        .getValue();
 
                 mTxtFacilityName.setText(FacName);
                 mTxtOpeningTime.setText(OpeningTime);
@@ -65,7 +96,8 @@ public class FacilityDetailsActivity extends AppCompatActivity {
                     @Override
                     public void onError(Exception e) {
 
-                        Picasso.get().load(FacilityImageUrl).placeholder(R.drawable.new_picture).into(mImgFacilityImage);
+                        Picasso.get().load(FacilityImageUrl).placeholder(R.drawable.new_picture)
+                                .into(mImgFacilityImage);
 
                     }
 
